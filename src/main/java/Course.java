5a5c59c7154a149cab21b926741fb2dd59775773
ShaiAshCollege464 <81906487+ShaiAshCollege464@users.noreply.github.com>
2023-06-20 22:56:21 +0300

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -2,22 +2,27 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Course {
     private String name;
-    private ConcurrentLinkedQueue<Student> students = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<StudentData> students = new ConcurrentLinkedQueue<>();
 
     public Course(String name) {
         this.name = name;
     }
 
+
     public void addStudent(Student student) {
-        students.add(student);
+        StudentData studentData = new StudentData(student);
+        students.add(studentData);
     }
 
     public void removeStudent(Student student) {
-        students.remove(student);
+        StudentData studentData = new StudentData(student);
+        students.remove(studentData);
     }
-
     public String getName() {
         return name;
     }
 
+    public ConcurrentLinkedQueue<StudentData> getStudents() {
+        return students;
+    }
 }
